a13816d9f7335bc836ac16ce29117265988d2b05
Davide Ferrari <davide@billymob.com>
2017-02-02 15:19:33 +0100

    Added more params as ENV
    - Set Graphite global prefix param via an ENV variable
    - Set the flush interval via an ENV variable (defaults to 10 seconds)
    - Rewritten a bit the statsd download code so it can be customized
    if another version comes out and you don't want to follow master

--- statsd/config.js
+++ statsd/config.js
@@ -2,12 +2,14 @@
  backends: [ "./backends/console", "./backends/graphite"],
  deleteIdleStats: true,
  graphite: {
-    legacyNamespace: false
+    legacyNamespace: false,
+    globalPrefix: "#STATSD_GRAPHITE_GLOBAL_PREFIX#"
  },
  graphiteHost: "#STATSD_GRAPHITE_HOST#",
  graphiteProtocol: "#STATSD_GRAPHITE_PROTOCOL#",
  graphitePort: #STATSD_GRAPHITE_PORT#,
  graphitePicklePort: #STATSD_GRAPHITE_PICKLE_PORT#,
+ flushInterval: "#STATSD_FLUSH_INTERVAL#",
  debug: #STATSD_DEBUG#,
  dumpMessages: #STATSD_DUMP_MESSAGES#
 }
--- statsd/startup.sh
+++ statsd/startup.sh
@@ -4,6 +4,7 @@
 [ -z "$STATSD_GRAPHITE_PROTOCOL" ] && STATSD_GRAPHITE_PROTOCOL="text"
 [ -z "$STATSD_GRAPHITE_PORT" ] && STATSD_GRAPHITE_PORT=2003
 [ -z "$STATSD_GRAPHITE_PICKLE_PORT" ] && STATSD_GRAPHITE_PICKLE_PORT=2004
+[ -z "$STATSD_FLUSH_INTERVAL" ] && STATSD_FLUSH_INTERVAL="10000"
 [ -z "$STATSD_DEBUG" ] && STATSD_DEBUG="false"
 [ -z "$STATSD_DUMP_MESSAGES" ] && STATSD_DUMP_MESSAGES="false"
 
@@ -11,7 +12,11 @@ sed -i "s/#STATSD_GRAPHITE_HOST#/$STATSD_GRAPHITE_HOST/" /statsd/config.js
 sed -i "s/#STATSD_GRAPHITE_PROTOCOL#/$STATSD_GRAPHITE_PROTOCOL/" /statsd/config.js
 sed -i "s/#STATSD_GRAPHITE_PORT#/$STATSD_GRAPHITE_PORT/" /statsd/config.js
 sed -i "s/#STATSD_GRAPHITE_PICKLE_PORT#/$STATSD_GRAPHITE_PICKLE_PORT/" /statsd/config.js
+sed -i "s/#STATSD_GRAPHITE_GLOBAL_PREFIX#/$STATSD_GRAPHITE_GLOBAL_PREFIX/" /statsd/config.js
+sed -i "s/#STATSD_FLUSH_INTERVAL#/$STATSD_FLUSH_INTERVAL/" /statsd/config.js
 sed -i "s/#STATSD_DEBUG#/$STATSD_DEBUG/" /statsd/config.js
 sed -i "s/#STATSD_DUMP_MESSAGES#/$STATSD_DUMP_MESSAGES/" /statsd/config.js
+
+cat /statsd/config.js
 
 node /statsd/stats.js /statsd/config.js
